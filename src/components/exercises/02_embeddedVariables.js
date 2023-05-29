090bf5fa295de0a152e91b2c722a7ce55bb413fe
Ian Blake <ian@ianbl8.com>
2023-05-29 11:42:26 +0100

    Exercise 2 completed

--- src/components/exercises/02_embeddedVariables.js
+++ src/components/exercises/02_embeddedVariables.js
@@ -3,8 +3,44 @@ import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 
 const Demo = () => {
+  const modules = [
+    {
+      "name": "Full Stack Web Development 1",
+      "lectures": 2,
+      "practicals": 2
+    },
+    {
+      "name": "Full Stack Web Development 2",
+      "lectures": 3,
+      "practicals": 2
+    }
+  ];
+  const course = ["HDip Computer Science"];
   return (
-      <h1>TODO</h1>
+    <div>
+      <h1>{`${course} - Modules table`}</h1>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody >
+          <tr>
+            <td>{modules[0].name}</td>
+            <td>{modules[0].lectures}</td>
+            <td>{modules[0].practicals}</td>
+          </tr>
+          <tr>
+            <td>{modules[1].name}</td>
+            <td>{modules[1].lectures}</td>
+            <td>{modules[1].practicals}</td>
+          </tr>
+        </tbody >
+      </table>
+    </div>
   );
 };
 
